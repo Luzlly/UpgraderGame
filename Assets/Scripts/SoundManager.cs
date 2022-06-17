2b90058ac836ef15ca67733448d4283a7526bba3
Luzlly <102785056+Luzlly@users.noreply.github.com>
2022-06-17 16:26:37 +1000

    Quick Fixes

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,11 +12,11 @@ public class SoundManager : MonoBehaviour
     {
         if(PlayerPrefs.HasKey("musicVolume"))
         {
-            PlayerPrefs.SetFloat("musicVolume", 1);
             Load();
         }
         else
         {
+            PlayerPrefs.SetFloat("musicVolume", 1);
             Load();
         }
     }
